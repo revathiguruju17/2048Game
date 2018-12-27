955725bd5e6ba25919d4df7e639a826668167ac4
revathi <revathiguruju17@gmail.com>
2018-12-27 12:33:08 +0530

    added few more testes for the merging and shifing in cell class

--- src/test/java/model/CellTest.java
+++ src/test/java/model/CellTest.java
@@ -2,8 +2,7 @@ package model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CellTest {
     @Test
@@ -39,8 +38,34 @@ class CellTest {
     void shouldShiftFromOneCellValueToAnotherCellValueWhenShiftMethodIsCalled(){
         Cell cell1 = new Cell( 0 );
         Cell cell2 = new Cell( 8 );
-        cell1.merge( cell2 );
+        cell1.shift( cell2 );
         assertEquals( 8,cell1.getValue() );
         assertEquals( 0,cell2.getValue() );
+    }
+
+    @Test
+    void checkingTwoCellsWhetherTheyCanBeShiftedOrNotForGivenCellOfValue0(){
+        Cell cell1 = new Cell(0);
+        assertTrue(cell1.checkCellsForShifting());
+    }
+
+    @Test
+    void checkingTwoCellsWhetherTheyCanBeShiftedOrNotForGivenCellOfValue16(){
+        Cell cell1 = new Cell(16);
+        assertFalse(cell1.checkCellsForShifting());
+    }
+
+    @Test
+    void checkingTwoCellsWhetherTheyCanBeMergedOrNotForGivenCellsOfValue8And8(){
+        Cell cell1 = new Cell(8);
+        Cell cell2 = new Cell(8);
+        assertTrue(cell1.checkCellsForMerging(cell2));
+    }
+
+    @Test
+    void checkingTwoCellsWhetherTheyCanBeShiftedOrNotForGivenCellOfValue16And8(){
+        Cell cell1 = new Cell(16);
+        Cell cell2 = new Cell(8);
+        assertFalse(cell1.checkCellsForMerging(cell2));
     }
 }
