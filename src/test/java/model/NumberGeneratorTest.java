4a20b0f6c6f54706d90b0da8491c91c815f6edf5
revathi <revathiguruju17@gmail.com>
2019-01-07 15:33:17 +0530

    added tests for number generator class

--- src/test/java/model/NumberGeneratorTest.java
+++ src/test/java/model/NumberGeneratorTest.java
@@ -1,5 +1,40 @@
 package model;
 
-public class NumberGeneratorTest {
-    
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class NumberGeneratorTest {
+    private NumberGenerator numberGenerator;
+
+    @BeforeEach
+    void init() {
+        numberGenerator = new NumberGenerator();
+    }
+
+    @Test
+    void shouldReturnTrueIfTheNumberIsWithinTheRange() {
+        int number = numberGenerator.getNumberWithinARange(15, 1);
+        assertTrue(number>0 && number<16);
+    }
+
+    @Test
+    void shouldReturnFalseIfTheNumberIsNotWithinTheRange() {
+        int number = numberGenerator.getNumberWithinARange(20, 5);
+        assertFalse(number<5 || number>20);
+    }
+
+    @Test
+    void shouldReturnTrueIfTheNumberIsWithinTheRangeAndIsPowerOf2() {
+        int number = numberGenerator.getNumberWithAPowerOf2(2, 1);
+        assertTrue(number>=2 && number<=4);
+    }
+
+    @Test
+    void shouldReturnFalseIfTheNumberIsNotWithinTheRangeAndIsPowerOf2() {
+        int number = numberGenerator.getNumberWithAPowerOf2(10, 1);
+        assertFalse(number<2 || number>1024);
+    }
 }
